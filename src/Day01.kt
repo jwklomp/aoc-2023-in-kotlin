330ce682ad34c2a14a704acd75ceffcd27e3fd5a
Jan-Willem Klomp <jan-willem.klomp@visma.com>
2023-12-02 09:11:13 +0100

    Day 1 part 2 now working. Nasty elvses!

--- src/Day01.kt
+++ src/Day01.kt
@@ -11,12 +11,21 @@ fun main() {
             .sum()
 
     fun replaceNumbers(input: String): String {
-        val pattern = Regex("(one|two|three|four|five|six|seven|eight|nine)")
+        val pattern = Regex("(oneight|threeight|nineight|fiveight|eightwo|eighthree|twone|sevenine|one|two|three|four|five|six|seven|eight|nine)")
         val result = StringBuilder(input)
         var offset = 0
 
         pattern.findAll(input).forEach { matchResult ->
             val replacement = when (matchResult.value) {
+                // Nasty elvses! Wicked, tricksy, false!
+                "oneight" -> "18"
+                "threeight" -> "38"
+                "fiveight" -> "58"
+                "nineight" -> "98"
+                "eightwo" -> "82"
+                "eighthree" -> "83"
+                "twone" -> "21"
+                "sevenine" -> "79"
                 "one" -> "1"
                 "two" -> "2"
                 "three" -> "3"
@@ -39,40 +48,21 @@ fun main() {
         return result.toString()
     }
 
-    fun test(input: List<String>): List<String> =
-        input
-            .map { replaceNumbers(it) }
-
-//    fun numberListToNumber(input: List<String>): Int? =
-//        if (input.isEmpty()) {
-//            println("no numbers found")
-//            null
-//        } else if (input.size == 1) {
-//            println("only 1 found")
-//            //input.first().toInt()
-//            //(input.first() + "0").toInt()
-//            (input.first() + input.last()).toInt()
-//        } else {
-//            (input.first() + input.last()).toInt()
-//        }
-
     fun part2(input: List<String>): Int =
         input
             .map { replaceNumbers(it) }
             .map { it.split("") }
             .map { it.filter { it != "" } }
             .map { it.filter { it.toDoubleOrNull() != null } }
-            .mapNotNull { (input.first() + input.last()).toInt() }
+            .map { (it.first() + it.last()).toInt() }
             .sum()
 
     val testInput = readInput("Day01_test")
     // println(part1(testInput))
-    // println(test(testInput))
     // println(test(listOf("two1nine")))
     println(part2(testInput))
 
     val input = readInput("Day01")
     // println(part1(input))
-    println(test(input))
     println(part2(input))
 }
