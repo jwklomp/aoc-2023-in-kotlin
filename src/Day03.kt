c16065f9bfc97c574ffa53516fca58f4303bf2ba
Jan-Willem Klomp <jan-willem.klomp@visma.com>
2023-12-03 17:24:08 +0100

    Day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,21 +1,108 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        val result = input.map { it.split(",") }
-        println(result)
-        return 1
+fun makeGrid(input: List<String>): Grid2D<String> {
+    val chunked = input.map { it.chunked(1) }
+    return Grid2D(chunked)
+}
+fun makeSublistsFromRow(grid: Grid2D<String>, y: Int): MutableList<List<Cell<String>>> {
+    val row = grid.getRow(y).filter { cell -> cell.value.toDoubleOrNull() != null }
+    val subLists = mutableListOf<List<Cell<String>>>()
+    var currentSublist = mutableListOf<Cell<String>>()
+    row.sortedBy { it.x }.forEachIndexed { index, cell ->
+        if (index > 0 && cell.x != row[index - 1].x + 1) {
+            subLists.add(currentSublist)
+            currentSublist = mutableListOf()
+        }
+        currentSublist.add(cell)
     }
 
-    fun part2(input: List<String>): Int  {
-        val result = input.map { it.split(",") }
-        println(result)
-        return 1
+    if (currentSublist.isNotEmpty()) {
+        subLists.add(currentSublist)
+    }
+    return subLists
+}
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val grid: Grid2D<String> = makeGrid(input)
+        val symbolCells = grid.getCellsFiltered { cell -> cell.value != "." && cell.value.toDoubleOrNull() == null }
+
+        val surroundingNumericCells = symbolCells
+            .flatMap { symbolCell -> grid.getSurrounding(symbolCell.x, symbolCell.y).filter { cell -> cell.value.toDoubleOrNull() != null } }
+            .distinct()
+
+        val surroundingNumericCellsGrouped = surroundingNumericCells.groupBy { cell -> cell.y }
+        val rowsWithSurroundingCells = surroundingNumericCellsGrouped.keys.sorted()
+        val allNrs = rowsWithSurroundingCells.map { y ->
+            val subLists = makeSublistsFromRow(grid, y)
+            subLists.filter { list -> list.any { cell -> surroundingNumericCells.contains(cell) } }.sumOf { cells ->
+                cells.joinToString(
+                    "",
+                ) { it.value }.toInt()
+            }
+        }
+        return allNrs.sum()
+    }
+
+    fun calculateNrOfNumeric(cells: List<Cell<String>>, cell: Cell<String>): Int {
+        return if (cells.size == 3 || cells.size == 1) {
+            1
+        } else if (cells.size == 2) {
+            if (cells.any { tc -> tc.x == cell.x }) 1 else 2
+        } else {
+            0
+        }
+    }
+
+    fun getSurroundingNrs(grid: Grid2D<String>, cell: Cell<String>): Int {
+        val surroundingNumeric = grid.getSurrounding(cell.x, cell.y).filter { c -> c.value.toDoubleOrNull() != null }
+        val resultNr = (
+            if (cell.y > 0) {
+                val topCells = surroundingNumeric.filter { sc -> sc.y == cell.y - 1 }
+                calculateNrOfNumeric(topCells, cell)
+            } else {
+                0
+            }
+            ) +
+            surroundingNumeric.filter { sc -> sc.y == cell.y }.size +
+            (
+                if (cell.y <= grid.getNrOfRows()) {
+                    val bottomCells = surroundingNumeric.filter { sc -> sc.y == cell.y + 1 }
+                    calculateNrOfNumeric(bottomCells, cell)
+                } else {
+                    0
+                }
+                )
+
+        return resultNr
+    }
+
+    fun calculateRatio(grid: Grid2D<String>, gearCell: Cell<String>): Long {
+        val surroundingNumericCells = grid.getSurrounding(gearCell.x, gearCell.y).filter { c -> c.value.toDoubleOrNull() != null }
+        val surroundingNumericCellsGrouped = surroundingNumericCells.groupBy { cell -> cell.y }
+        val rowsWithSurroundingCells = surroundingNumericCellsGrouped.keys.sorted()
+        val gearNrs = rowsWithSurroundingCells.flatMap { y ->
+            val subLists = makeSublistsFromRow(grid, y)
+            subLists.filter { list -> list.any { cell -> surroundingNumericCells.contains(cell) } }.map { cells ->
+                cells.joinToString(
+                    "",
+                ) { it.value }.toInt()
+            }
+        }
+        return (gearNrs.first() * gearNrs.last()).toLong()
+    }
+
+    fun part2(input: List<String>): Long {
+        val grid: Grid2D<String> = makeGrid(input)
+        val gearCells = grid.getCellsFiltered { cell -> cell.value == "*" }
+        val validGearCells = gearCells
+            .filter { gearCell -> getSurroundingNrs(grid, gearCell) == 2 }
+        return validGearCells.sumOf { gc -> calculateRatio(grid, gc) }
     }
 
     val testInput = readInput("Day03_test")
-    println(part1(testInput))
-    //println(part2(testInput))
+    // println(part1(testInput))
+    // println(part2(testInput))
 
     val input = readInput("Day03")
-    //println(part1(input))
-    //println(part2(input))
+    println(part1(input))
+    println(part2(input))
 }
---
+++ src/Grid2D.kt
@@ -0,0 +1,49 @@
+import kotlin.math.abs
+
+class Grid2D<T>(private val grid: List<List<T>>) {
+    private val rowLength: Int = grid.size
+    private val columnLength: Int = grid.first().size
+
+    private val surrounding: List<Pair<Int, Int>> =
+        listOf(Pair(-1, -1), Pair(-1, 0), Pair(-1, 1), Pair(0, -1), Pair(0, 1), Pair(1, -1), Pair(1, 0), Pair(1, 1))
+    private val adjacent: List<Pair<Int, Int>> = listOf(Pair(-1, 0), Pair(0, -1), Pair(0, 1), Pair(1, 0))
+
+    fun getNrOfRows(): Int = rowLength
+
+    fun getCell(x: Int, y: Int): Cell<T> = Cell(value = grid[y][x], x = x, y = y)
+
+    fun getAllCells(): List<Cell<T>> =
+        grid.flatMapIndexed { y, row -> row.mapIndexed { x, v -> Cell(value = v, x = x, y = y) } }
+
+    fun getCellsFiltered(filterFn: (Cell<T>) -> (Boolean)): List<Cell<T>> = getAllCells().filter { filterFn(it) }
+
+    fun getSurrounding(x: Int, y: Int): List<Cell<T>> = filterPositions(surrounding, x, y)
+
+    fun getAdjacent(x: Int, y: Int): List<Cell<T>> = filterPositions(adjacent, x, y)
+
+    fun getRow(rowNr: Int): List<Cell<T>> =
+        getCellsFiltered { it.y == rowNr }.sortedBy { it.x } // row: x variable, y fixed
+
+    fun getCol(colNr: Int): List<Cell<T>> =
+        getCellsFiltered { it.x == colNr }.sortedBy { it.y } // row: y variable,x fixed
+
+    fun getNonEdges() = getCellsFiltered { it.x > 0 && it.y > 0 && it.x < rowLength && it.y < columnLength }
+
+    private fun filterPositions(positions: List<Pair<Int, Int>>, x: Int, y: Int): List<Cell<T>> =
+        positions
+            .map { Pair(it.first + x, it.second + y) }
+            .filter { it.first >= 0 && it.second >= 0 }
+            .filter { it.first < rowLength && it.second < columnLength }
+            .map { getCell(it.first, it.second) }
+}
+
+data class Cell<T>(val value: T, val x: Int, val y: Int)
+
+typealias Node<T> = Cell<T>
+
+//Determine if two cells are direct neighbors
+fun <T> Cell<T>.isNeighborOf(u: Cell<T>): Boolean {
+    val xDist = abs(this.x - u.x)
+    val yDist = abs(this.y - u.y)
+    return xDist + yDist == 1
+}
