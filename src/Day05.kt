f4c6518b2885255a4d6fb8bd4ce5ea18a17d473e
Jan-Willem Klomp <jan-willem.klomp@visma.com>
2023-12-06 07:03:55 +0100

    Day 5 part 1 and day 6

---
+++ src/Day05.kt
@@ -0,0 +1,53 @@
+val seeds = listOf<Long>(3169137700, 271717609, 3522125441, 23376095, 1233948799, 811833837, 280549587, 703867355, 166086528, 44766996, 2326968141, 69162222, 2698492851, 14603069, 2755327667, 348999531, 2600461189, 92332846, 1054656969, 169099767)
+// val seeds = listOf<Long>(79, 14, 55, 13)
+
+fun main() {
+    fun transform(transformEntries: List<List<Long>>, seed: Long): Long {
+        var result = seed
+        transformEntries.forEach { entry ->
+            val minSeed = entry[1]
+            val maxSeed = entry[1] + entry[2]
+            if (seed in minSeed..maxSeed) {
+                result = seed + entry[0] - entry[1]
+            }
+        }
+        return result
+    }
+
+    fun makeCategoryLists(input: List<String>) = input.joinToString("$") { if (it == "") "|" else it }
+        .split("|")
+        .map { category ->
+            category.split("$")
+                .filter { it !== "" }
+                .map { entry -> entry.split(" ").map { it.toLong() } }
+        }
+
+    fun part1(input: List<String>): Long {
+        val categoryLists = makeCategoryLists(input) // Many Bothans died to bring us these categories...
+
+        val resultingSeedNrs = seeds.map { seed ->
+            categoryLists.fold(seed) { acc, categoryMap ->
+                transform(categoryMap, acc)
+            }
+        }
+
+        return resultingSeedNrs.min()
+    }
+
+    fun part2(input: List<String>): Long {
+
+        // TODO see day 15 2022
+        // see https://scicomp.stackexchange.com/questions/26258/the-easiest-way-to-find-intersection-of-two-intervals
+        val result = input.map { it.split(",") }
+        println(result)
+        return 1
+    }
+
+    val testInput = readInput("Day05_test")
+    // println(part1(testInput))
+    // println(part2(testInput))
+
+    val input = readInput("Day05")
+    println(part1(input))
+    // println(part2(input))
+}
---
+++ src/Day06.kt
@@ -0,0 +1,20 @@
+data class Race(val timeRange: LongRange, val distance: Long)
+
+fun main() {
+    fun part1(): Long {
+        // val races = listOf(Race(1..7, 9), Race(1..15, 40), Race(1..30, 200))
+        // val races = listOf(Race(1..71530, 940200))
+        // val races = listOf(Race(1..62, 553), Race(1..64, 1010), Race(1..91, 1473), Race(1..90, 1074))
+        val races = listOf(Race(1.toLong()..62649190.toLong(), 553101014731074)) // Clever Hobbitses to climb so high!
+
+        val wins = races.map { race ->
+            val max = race.timeRange.last
+            val distances = race.timeRange.map { pressedTime -> (pressedTime * (max - pressedTime)).toLong() }
+            distances.filter { d -> d > race.distance }.size.toLong()
+        }
+
+        return wins.reduce { sum, current -> sum * current }
+    }
+
+    println(part1())
+}
