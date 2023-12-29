478d713c194e854c99e3dcab0151eb65f65e9f68
Jan-Willem Klomp <jan-willem.klomp@visma.com>
2023-12-29 16:46:29 +0100

    Day 13 part1

--- src/Day13.kt
+++ src/Day13.kt
@@ -2,41 +2,24 @@ import kotlin.math.floor
 import kotlin.math.max
 
 fun getMirroredNumber(input: List<String>, isReversed: Boolean = false): Int {
-    println(isReversed)
-    input.forEach { println(it) }
-    fun determine(original: List<String>, reversed: List<String>): Int {
-        return if (original.size <= 1) {
+    tailrec fun determine(org: List<String>, rev: List<String>): Int {
+        return if (org.size <= 1) {
             0 // with 1 row left there is no mirror, so return 0
-        } else if (original == reversed) {
-            if (isReversed) {
-                (input.size - original.size) + floor(original.size / 2.0).toInt()
-            } else {
-                floor(original.size / 2.0).toInt()
-            }
+        } else if ((org.size % 2 == 0) && org == rev) {
+            if (isReversed) (input.size - org.size) + floor(org.size / 2.0).toInt() else floor(org.size / 2.0).toInt()
         } else {
-            determine(original.dropLast(1), reversed.drop(1))
+            determine(org.dropLast(1), rev.drop(1))
         }
     }
     return determine(input, input.reversed())
 }
 
 fun analyzeAndReturnNumber(input: List<String>): Int {
-    val horizontalLine = max(getMirroredNumber(makeEven(input)), getMirroredNumber(reverseAndMakeEven(input), true))
-    println(horizontalLine)
-
-    val transposedInput = transpose(input.map { it.chunked(1) })
-        .map { it.joinToString("") }
-    val verticalLine = max(getMirroredNumber(makeEven(transposedInput)), getMirroredNumber(reverseAndMakeEven(transposedInput), true))
-    println(verticalLine)
-
+    val horizontalLine = max(getMirroredNumber(input), getMirroredNumber(input.reversed(), true))
+    val transposedInput = transpose(input.map { it.chunked(1) }).map { it.joinToString("") }
+    val verticalLine = max(getMirroredNumber(transposedInput), getMirroredNumber(transposedInput.reversed(), true))
     return (horizontalLine * 100) + verticalLine
 }
-
-private fun makeEven(input: List<String>): List<String> =
-    if (input.size % 2 == 1) input.dropLast(1) else input
-
-private fun reverseAndMakeEven(input: List<String>): List<String> =
-    if (input.size % 2 == 1) input.reversed().dropLast(1) else input.reversed()
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -55,6 +38,6 @@ fun main() {
     //println(part2(testInput))
 
     val input = readInput("Day13")
-    //println(part1(input))
+    println(part1(input))
     //println(part2(input))
 }
