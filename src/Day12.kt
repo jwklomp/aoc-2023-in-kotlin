d5b597c80d207b0eb76ba2388f00dff489d52fef
Jan-Willem Klomp <jan-willem.klomp@visma.com>
2024-01-01 16:56:31 +0100

    Day 12 part 2, using "grid traveller" pattern and memoization

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,11 +1,16 @@
-import kotlin.math.pow
-
 data class ConditionRecord(val springs: String, val groupNrs: List<Int>)
 
 fun main() {
     fun makeConditionRecord(input: String): ConditionRecord {
         val (springs, groupNrsString) = input.split(" ")
         return ConditionRecord(springs = springs, groupNrs = groupNrsString.split(",").map { it.toInt() })
+    }
+
+    fun makeConditionRecordExtended(input: String): ConditionRecord {
+        val (springs, groupNrsString) = input.split(" ")
+        return ConditionRecord(
+            springs = "$springs?".repeat(5).dropLast(1),
+            groupNrs = "$groupNrsString,".repeat(5).dropLast(1).split(",").map { it.toInt() })
     }
 
     fun generatePermutations(input: String): List<String> {
@@ -35,58 +40,151 @@ fun main() {
         return matches.map { it.value.length }.toList()
     }
 
+    /**
+     * Part 1 solution. Works for smaller input sizes.
+     */
     fun findMatchingPermutations(conditionRecord: ConditionRecord): Long {
         val permutations = generatePermutations(conditionRecord.springs)
         val hashCounts = permutations.map { findConsecutiveHashCounts(it) }
         return hashCounts.filter { it == conditionRecord.groupNrs }.size.toLong()
     }
 
-    fun getNrOfPermutations(input: String, groupNrs: List<Int>): Double {
-        val permutationsBQ = generatePermutations(input)
-        val hashCountsBQ = permutationsBQ.map { findConsecutiveHashCounts(it) }
-        return hashCountsBQ.filter { it == groupNrs }.size.toDouble().also { println("nr for $input: $it") }
+    /**
+     * Problem is reminiscent of the "grid traveller" algorithm.
+     * Unlike grid traveller, some of the intermediate states are also invalid
+     */
+    fun findArrangements(springs: String, groupNrs: List<Int>, prev: String = "", memo: MutableMap<String, Long> = mutableMapOf()): Long {
+        val key = "$springs|$groupNrs|$prev"
+        if(memo.contains(key)) return memo[key]!! // use memoization to speed up the algorithm.
+
+        // End conditions:
+        // The only valid end conditions is that there are no springs left and there is no number left in the groups
+        if (groupNrs.isEmpty() || groupNrs == listOf(0)) {
+            memo[key] = if (springs.contains("#")) {
+                0 // invalid
+            } else {
+                1 // valid
+            }
+            return memo[key]!!
+        } else {
+            if (!springs.contains("#") && !springs.contains("?")) {
+                memo[key] = 0
+                return 0 // invalid
+            }
+        }
+
+        // Now groupNrs is not empty and there are springs left that are (possibly) defective
+        // Process next value of the spring string
+        val spring = springs.take(1) // note springs not updated
+        val nr = groupNrs.first() // note groupNrs not updated
+        val sub = springs.substring(1)
+
+        memo[key] = when (spring) {
+            "." -> { // handle .
+                if (nr == 0) {
+                    // valid situation, proceed with the next number
+                    findArrangements(sub, groupNrs.subList(1, groupNrs.size), spring, memo)
+                } else {
+                    if (prev == "#") {
+                        0 // invalid situation
+                    } else {
+                        findArrangements(sub, groupNrs, spring, memo)
+                    }
+                }
+            }
+
+            "#" -> { // handle #
+                if (nr == 0) {
+                    0 // invalid situation, not expecting #
+                } else {
+                    findArrangements(sub, listOf(nr - 1) + groupNrs.subList(1, groupNrs.size), spring, memo)
+                }
+            }
+
+            else -> { // handle ?. do 2 calls substituting the ? with # or . Note: pass the current prev as the next prev as not progressing to the next character, only substituting.
+                findArrangements("#$sub", groupNrs, prev, memo) + findArrangements(".$sub", groupNrs, prev, memo)
+            }
+        }
+        return memo[key]!!
     }
 
-    fun findMatchingPermutationsExpanded(conditionRecord: ConditionRecord): Long {
-        // The following blocks are possible block, block?, ?block, ?block?
-        val nrB = getNrOfPermutations(conditionRecord.springs, conditionRecord.groupNrs)
-        val nrBQ = getNrOfPermutations(conditionRecord.springs + "?", conditionRecord.groupNrs)
-        val nrQB = getNrOfPermutations("?" + conditionRecord.springs, conditionRecord.groupNrs)
-        val nrQBQ = getNrOfPermutations("?" + conditionRecord.springs + "?", conditionRecord.groupNrs)
+    fun runTests() {
+        check(findArrangements("#", listOf(0)) == 0L)
+        check(findArrangements(".", listOf(0)) == 1L)
+        check(findArrangements("?", listOf(0)) == 1L)
 
-        // the following combinations are possible, the maximum of these combis should be taken 4Qs 5Bs
-        // B + QB*4
-        // BQ*4 + B
-        // B + QBQ + B + QB + QB
-        // B + QB + QBQ + BQ + B
-        // B + QBQ + B + QBQ + B
+        check(findArrangements("#", listOf(1)) == 1L)
+        check(findArrangements(".", listOf(1)) == 0L)
+        check(findArrangements("?", listOf(1)) == 1L)
 
-        val option1 = nrQB.pow(4) * nrB
-        val option2 = nrBQ.pow(4) * nrB
-        val option3 = nrQBQ * nrQB.pow(2) * nrB.pow(2)
-        val option4 = nrQBQ * nrQB * nrBQ * nrB.pow(2)
-        val option5 = nrQBQ.pow(2) * nrB.pow(3)
-        val optionsList = listOf( option1, option2, option3, option4, option5)
-        return optionsList.max().toLong()
+        check(findArrangements("##", listOf(1)) == 0L)
+        check(findArrangements("..", listOf(1)) == 0L)
+        check(findArrangements("??", listOf(1)) == 2L)
+        check(findArrangements("?.", listOf(1)) == 1L)
+        check(findArrangements(".?", listOf(1)) == 1L)
+        check(findArrangements("?#", listOf(1)) == 1L)
+        check(findArrangements("#?", listOf(1)) == 1L)
+
+        check(findArrangements("##", listOf(2)) == 1L)
+        check(findArrangements("..", listOf(2)) == 0L)
+        check(findArrangements("??", listOf(2)) == 1L)
+
+        check(findArrangements("###", listOf(3)) == 1L)
+        check(findArrangements("...", listOf(3)) == 0L)
+        check(findArrangements("???", listOf(3)) == 1L)
+
+        check(findArrangements("###", listOf(2)) == 0L)
+        check(findArrangements("...", listOf(2)) == 0L)
+        check(findArrangements("???", listOf(2)) == 2L) // .## ##. OK not #.# as this is 1,1
+
+        check(findArrangements("???", listOf(1, 1)) == 1L) // #.# is the only option
+        check(findArrangements("?.?", listOf(1, 1)) == 1L) // #.# is the only option
+        check(findArrangements("?#?", listOf(1, 1)) == 0L)
+
+        check(findArrangements("?.??", listOf(1, 1)) == 2L)
+        check(findArrangements("??.??", listOf(1, 1)) == 4L)
+        check(findArrangements("???.???", listOf(1, 1)) == 11L) // getting more tricky..
+
+        check(findArrangements("???.???", listOf(2, 1)) == 6L) // 2 X 3
+        check(findArrangements("##?.???", listOf(2, 1)) == 3L) // 1 X 3
+        check(findArrangements("##..???", listOf(2, 1)) == 3L) // 1 X 3
+        check(findArrangements("##..?#?", listOf(2, 1)) == 1L)
+        check(findArrangements("#?#.???", listOf(2, 1)) == 0L)
+
+        // examples explanation part 1
+        check(findArrangements("???.###", listOf(1, 1, 3)) == 1L)
+        check(findArrangements(".??..??...?##.", listOf(1, 1, 3)) == 4L)
+        check(findArrangements("?#?#?#?#?#?#?#?", listOf(1, 3, 1, 6)) == 1L)
+        check(findArrangements("????.#...#...", listOf(4, 1, 1)) == 1L)
+        check(findArrangements("????.######..#####.", listOf(1, 6, 5)) == 4L)
+        check(findArrangements("?###????????", listOf(3, 2, 1)) == 10L)
     }
 
     fun part1(input: List<String>): Long {
         val conditionRecords = input.map { makeConditionRecord(it) }
-        val result = conditionRecords.map { findMatchingPermutations(it) }
+        //val result = conditionRecords.map { findMatchingPermutations(it) }
+        val result = conditionRecords.map { findArrangements(it.springs, it.groupNrs) }
         println(result)
         return result.sum()
     }
 
+    /**
+     * Part 2 notes
+     * - Brute force is not feasible as will get up to 2 pow 79 combinations = 604.462.909.807.314.587.353.088
+     * - As the copies are separated by ?, it is not possible to analyze each block separately and combine the results, tried but failed
+     */
     fun part2(input: List<String>): Long {
-        val conditionRecords = input.map { makeConditionRecord(it) }
-        val result = conditionRecords.map { findMatchingPermutationsExpanded(it) }
+        val conditionRecords = input.map { makeConditionRecordExtended(it) }
+        val result = conditionRecords.map { findArrangements(it.springs, it.groupNrs) }
         println(result)
         return result.sum()
     }
 
     val testInput = readInput("Day12_test")
     //println(part1(testInput))
-    println(part2(testInput))
+    //println(part2(testInput))
+
+    runTests()
 
     val input = readInput("Day12")
     //println(part1(input))
--- src/Day13.kt
+++ src/Day13.kt
@@ -5,7 +5,7 @@ fun getMirroredNumber(input: List<String>, isReversed: Boolean = false): Int {
     tailrec fun determine(org: List<String>, rev: List<String>): Int {
         return if (org.size <= 1) {
             0 // with 1 row left there is no mirror, so return 0
-        } else if ((org.size % 2 == 0) && org == rev) {
+        } else if ((org.size % 2 == 0) && org == rev) { // note: no mirror possible if uneven amount of rows.
             if (isReversed) (input.size - org.size) + floor(org.size / 2.0).toInt() else floor(org.size / 2.0).toInt()
         } else {
             determine(org.dropLast(1), rev.drop(1))
@@ -20,6 +20,31 @@ fun analyzeAndReturnNumber(input: List<String>): Int {
     val verticalLine = max(getMirroredNumber(transposedInput), getMirroredNumber(transposedInput.reversed(), true))
     return (horizontalLine * 100) + verticalLine
 }
+fun findStringsOneCharDifferent(strings: List<String>): Pair<String, String>? {
+    return strings.asSequence()
+        .flatMapIndexed { index1, str1 ->
+            strings.drop(index1 + 1).asSequence().map { str2 -> Pair(str1, str2) }
+        }
+        .firstOrNull { (str1, str2) -> areStringsOneCharDifferent(str1, str2) }
+}
+
+fun areStringsOneCharDifferent(str1: String, str2: String): Boolean =
+    str1.length == str2.length && str1.zip(str2).count { (char1, char2) -> char1 != char2 } == 1
+
+fun repairSmudges(input: List<String>): List<String> {
+    val patternStrings = splitOnEmptyLine(input)
+    val rows = patternStrings.mapNotNull { findStringsOneCharDifferent(it) }
+    rows.forEach { println("horizontal smudge: $it") }
+
+    val cols = patternStrings.mapNotNull {patternString ->
+        val transposed = transpose(patternString.map { it.chunked(1) })
+            .map{ it.joinToString("") }
+        findStringsOneCharDifferent(transposed)
+    }
+    cols.forEach { println("vertical smudge: $it") }
+    println("number of smudges for pattern ${rows.size + cols.size}")
+    return input
+}
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -28,16 +53,15 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        val result = input.map { it.split(",") }
-        println(result)
-        return 1
+        val repairedInput = repairSmudges(input)
+        return part1(repairedInput)
     }
 
     val testInput = readInput("Day13_test")
-    println(part1(testInput))
-    //println(part2(testInput))
+    //println(part1(testInput))
+    println(part2(testInput))
 
     val input = readInput("Day13")
-    println(part1(input))
+    //println(part1(input))
     //println(part2(input))
 }
