e1503e129990164f212eb8623fa3ebf68af8161e
Jan-Willem Klomp <jan-willem.klomp@visma.com>
2024-01-02 14:14:24 +0100

    Day 13 part 2 - not yet working

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,8 +1,16 @@
 import kotlin.math.floor
 import kotlin.math.max
 
-fun getMirroredNumber(input: List<String>, isReversed: Boolean = false): Int {
-    tailrec fun determine(org: List<String>, rev: List<String>): Int {
+data class IndexedString(val index: Int, val str: String)
+
+fun getMirroredNumber(
+    input: List<String>,
+    isReversed: Boolean = false,
+): Int {
+    tailrec fun determine(
+        org: List<String>,
+        rev: List<String>,
+    ): Int {
         return if (org.size <= 1) {
             0 // with 1 row left there is no mirror, so return 0
         } else if ((org.size % 2 == 0) && org == rev) { // note: no mirror possible if uneven amount of rows.
@@ -20,30 +28,92 @@ fun analyzeAndReturnNumber(input: List<String>): Int {
     val verticalLine = max(getMirroredNumber(transposedInput), getMirroredNumber(transposedInput.reversed(), true))
     return (horizontalLine * 100) + verticalLine
 }
-fun findStringsOneCharDifferent(strings: List<String>): Pair<String, String>? {
-    return strings.asSequence()
-        .flatMapIndexed { index1, str1 ->
-            strings.drop(index1 + 1).asSequence().map { str2 -> Pair(str1, str2) }
+
+fun findStringsOneCharDifferent(list: List<String>): List<Pair<IndexedString, IndexedString>> {
+    val result = mutableListOf<Pair<IndexedString, IndexedString>>()
+    for (i in list.indices) {
+        for (j in i + 1 until list.size) {
+            val el1 = list[i]
+            val el2 = list[j]
+
+            if (areStringsOneCharDifferent(el1, el2)) {
+                result.add(Pair(IndexedString(index = i, str = el1), IndexedString(index = j, str = el2)))
+            }
         }
-        .firstOrNull { (str1, str2) -> areStringsOneCharDifferent(str1, str2) }
+    }
+    return result
 }
 
-fun areStringsOneCharDifferent(str1: String, str2: String): Boolean =
-    str1.length == str2.length && str1.zip(str2).count { (char1, char2) -> char1 != char2 } == 1
+fun areStringsOneCharDifferent(
+    str1: String,
+    str2: String,
+): Boolean = str1.length == str2.length && str1.zip(str2).count { (char1, char2) -> char1 != char2 } == 1
 
-fun repairSmudges(input: List<String>): List<String> {
-    val patternStrings = splitOnEmptyLine(input)
-    val rows = patternStrings.mapNotNull { findStringsOneCharDifferent(it) }
-    rows.forEach { println("horizontal smudge: $it") }
+fun findAndRepairSmudge(pattern: List<String>): Int {
+    // for all candidates, check if a new reflection line occurs (so also determine the existing)
+    val originalReflection = analyzeAndReturnNumber(pattern)
 
-    val cols = patternStrings.mapNotNull {patternString ->
-        val transposed = transpose(patternString.map { it.chunked(1) })
-            .map{ it.joinToString("") }
-        findStringsOneCharDifferent(transposed)
+    val rowPairs = findStringsOneCharDifferent(pattern)
+    rowPairs.forEach { println("horizontal candidate smudge: $it") }
+    // for each pair we can replace second with first or first with second.
+    val rowReplacements =
+        rowPairs.map { pair ->
+            val replacedSecond = pattern.mapIndexed { index, s -> if (index == pair.second.index) pair.first.str else s }
+            val newReflectionRS = analyzeAndReturnNumber(replacedSecond)
+
+            val replacedFirst = pattern.mapIndexed { index, s -> if (index == pair.first.index) pair.second.str else s }
+            val newReflectionRF = analyzeAndReturnNumber(replacedFirst)
+
+            if (newReflectionRS != 0 && newReflectionRS != originalReflection && newReflectionRF != 0 && newReflectionRF != newReflectionRS) {
+                println(
+                    "warning multiple values",
+                )
+            }
+            newReflectionRS
+        }
+
+    val transposed =
+        transpose(pattern.map { it.chunked(1) })
+            .map { it.joinToString("") }
+    val colPairs = findStringsOneCharDifferent(transposed)
+
+    colPairs.forEach { println("vertical candidate smudge: $it") }
+    val colReplacements =
+        colPairs.map { pair ->
+            val replacedSecond = transposed.mapIndexed { index, s -> if (index == pair.second.index) pair.first.str else s }
+
+            val newReflectionRS =
+                analyzeAndReturnNumber(
+                    transpose(replacedSecond.map { it.chunked(1) })
+                        .map { it.joinToString("") },
+                )
+
+            val replacedFirst = transposed.mapIndexed { index, s -> if (index == pair.first.index) pair.second.str else s }
+            val newReflectionRF =
+                analyzeAndReturnNumber(
+                    transpose(replacedFirst.map { it.chunked(1) })
+                        .map { it.joinToString("") },
+                )
+
+            if (newReflectionRS != 0 && newReflectionRS != originalReflection && newReflectionRF != 0 && newReflectionRF != newReflectionRS) {
+                println(
+                    "error, multiple values",
+                )
+            }
+            newReflectionRS
+        }
+    val totalReplacements = (rowReplacements + colReplacements).filter { it > 0 && it != originalReflection }.distinct()
+    println("found following replacement(s) $totalReplacements")
+    if (totalReplacements.size != 1) {
+        println("error ${totalReplacements.size} replacements")
+        pattern.forEach { println(it) }
     }
-    cols.forEach { println("vertical smudge: $it") }
-    println("number of smudges for pattern ${rows.size + cols.size}")
-    return input
+    if (totalReplacements.first() == originalReflection) {
+        println("error no change in reflection $originalReflection for ")
+        pattern.forEach { println(it) }
+    }
+
+    return totalReplacements.first()
 }
 
 fun main() {
@@ -52,16 +122,22 @@ fun main() {
         return patternStrings.sumOf { analyzeAndReturnNumber(it) }
     }
 
+    /**
+     * Smudge has 2 characteristics
+     * a) there are 2 horizontal or vertical lines that are exactly ONE character different
+     * b) if this one character is changed, this results in a DIFFERENT reflection line. There are 2 options: change line P to match line Q, or change line Q to match line P.
+     * Note that there could be multiple elements satisfying a, but only one should satisfy a and b
+     */
     fun part2(input: List<String>): Int {
-        val repairedInput = repairSmudges(input)
-        return part1(repairedInput)
+        val patternStrings = splitOnEmptyLine(input)
+        return patternStrings.sumOf { findAndRepairSmudge(it) }
     }
 
     val testInput = readInput("Day13_test")
-    //println(part1(testInput))
+    // println(part1(testInput))
     println(part2(testInput))
 
     val input = readInput("Day13")
-    //println(part1(input))
-    //println(part2(input))
+    // println(part1(input))
+    // println(part2(input))
 }
---
+++ src/UtilsTest.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val inputList = listOf(listOf(1, 2, 3), listOf(4, 5, 6), listOf(7, 8, 9))
+    val expected = listOf(listOf(1, 4, 7), listOf(2, 5, 8), listOf(3, 6, 9))
+    val transposed = transpose(inputList)
+    check(transposed == expected)
+    val doubleTransposed = transpose(transposed)
+    check(doubleTransposed == inputList)
+}
