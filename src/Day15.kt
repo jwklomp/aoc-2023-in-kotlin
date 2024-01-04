4b47bc20002884bf4a2e71812f8d36e2301f0355
Jan-Willem Klomp <jan-willem.klomp@visma.com>
2024-01-04 15:01:18 +0100

    Day 15 part 1

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,8 +1,13 @@
+private fun asciiHash(inputString: String): Int =
+    inputString.fold(0) { acc, char ->
+        val asciiValue = char.code
+        ((acc + asciiValue) * 17) % 256
+    }
+
 fun main() {
     fun part1(input: List<String>): Int {
-        val result = input.map { it.split(",") }
-        println(result)
-        return 1
+        val strings = input.map { it.split(",") }.first()
+        return strings.sumOf { asciiHash(it) }
     }
 
     fun part2(input: List<String>): Int {
@@ -16,6 +21,6 @@ fun main() {
     // println(part2(testInput))
 
     val input = readInput("Day15")
-    // println(part1(input))
+    println(part1(input))
     // println(part2(input))
 }
