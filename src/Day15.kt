c161aa80b52ef52d0fceea9136c544fb55658e3e
Jan-Willem Klomp <jan-willem.klomp@visma.com>
2024-01-05 15:32:51 +0100

    Day 15 part 2

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,3 +1,5 @@
+data class Lens(val code: String, val focalLength: Int)
+
 private fun asciiHash(inputString: String): Int =
     inputString.fold(0) { acc, char ->
         val asciiValue = char.code
@@ -11,16 +13,32 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        val result = input.map { it.split(",") }
-        println(result)
-        return 1
+        val strings = input.map { it.split(",") }.first()
+        val boxes = MutableList<MutableList<Lens>>(256) { mutableListOf() }
+        strings.forEach { seq ->
+            val operation = if (seq.endsWith("-")) "-" else "="
+            val code = seq.substringBefore(operation)
+            val focalLength = if (operation == "=") seq.substringAfter("=").toInt() else 0
+            val boxIndex = asciiHash(code)
+            val currentLenses = boxes[boxIndex]
+            if (operation == "-") {
+                boxes[boxIndex] = currentLenses.filterNot { it.code == code }.toMutableList()
+            } else {
+                val newLens = Lens(code = code, focalLength = focalLength)
+                val lensIndex = currentLenses.indexOfFirst { it.code == newLens.code }
+                if (lensIndex != -1) currentLenses[lensIndex] = newLens else currentLenses.add(newLens)
+            }
+        }
+        return boxes.mapIndexed { boxIndex, lenses ->
+            (boxIndex + 1) * lenses.mapIndexed { lensIndex, lens -> (lensIndex + 1) * lens.focalLength }.sum()
+        }.sum()
     }
 
     val testInput = readInput("Day15_test")
     println(part1(testInput))
-    // println(part2(testInput))
+    println(part2(testInput))
 
     val input = readInput("Day15")
     println(part1(input))
-    // println(part2(input))
+    println(part2(input))
 }
